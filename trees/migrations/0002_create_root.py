f7d1e94e0178f53c29722a247d56fabb1bedb5b9
Brian Lutz <brian.lutz9@icloud.com>
2019-11-17 11:41:39 -0800

    Add default values for lft, rght, tree_id and level
    Because of the way the model is imported, it doesn't fill in the values for lft, rght, and tree_id (which cannot be None) like the model manager does if it's used in a shell or outside of a migration. Setting the default values does the same as initializing one node as the root in a shell.

--- trees/migrations/0002_create_root.py
+++ trees/migrations/0002_create_root.py
@@ -3,7 +3,7 @@ from django.db import migrations
 
 def create_root(apps, schema_editor):
     TreeNode = apps.get_model('trees', 'TreeNode')
-    TreeNode.objects.create(is_root=True)
+    TreeNode.objects.create(is_root=True, lft=1, rght=2, tree_id=1, level=0)
 
 
 class Migration(migrations.Migration):
